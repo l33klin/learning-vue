cd5a4984fe5fd41a26111f1d14720d2022052413
Jann <l33klin@foxmail.com>
2023-04-03 23:25:57 +0800

    assert

--- hello-node/src/ts/index.ts
+++ hello-node/src/ts/index.ts
@@ -22,3 +22,19 @@ console.log(admin)
 
 import md5 from 'md5'
 console.log(md5('Hello World'))
+
+// 对单人或者多人打招呼
+function greet(name: string | string[]): string | string[] {
+    if (Array.isArray(name)) {
+        return name.map((n) => `Welcome, ${n}!`)
+    }
+    return `Welcome, ${name}!`
+}
+
+// 虽然已知此时应该是 string[]
+// 但 TypeScript 还是会认为这是 string | string[]
+const greetings = greet(['Petter', 'Tom', 'Jimmy']) as string[]
+
+// 会导致无法使用 join 方法
+const greetingSentence = greetings.join(' ')
+console.log(greetingSentence)
