e5fae8d5504a8c600123d079ff12a99241ce4aa0
Jann <l33klin@foxmail.com>
2023-04-03 23:49:16 +0800

    build ts module

--- hello-node/src/ts/index.ts
+++ hello-node/src/ts/index.ts
@@ -20,15 +20,7 @@ const admin: Admin = {
 
 console.log(admin)
 
-// 对单人或者多人打招呼
-function greet(name: string): string
-function greet(name: string[]): string[]
-function greet(name: string | string[]) {
-    if (Array.isArray(name)) {
-        return name.map((n) => `Welcome, ${n}!`)
-    }
-    return `Welcome, ${name}!`
-}
+import greet from "./greet"
 
 // 单个问候语
 const greeting = greet('Petter')
